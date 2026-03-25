f5bf1d7bbd5340beedf1c5ba2790731dd0dae5d2
JenniferAkinyi <jennifer.o.akinyi@gmail.com>
2026-03-25 14:42:43 +0300

    discover new groups

--- Frontend/study-sphere-frontend/src/components/Study Group/Components/DiscoverGroups.jsx
+++ Frontend/study-sphere-frontend/src/components/Study Group/Components/DiscoverGroups.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { fetchDiscoveredGroups } from "../../../services/api";
-import Avatar from "../../Authentication/Avatar";
 
 const DiscoverGroups = () => {
   const [groups, setGroups] = useState([]);
@@ -21,22 +20,6 @@ const DiscoverGroups = () => {
           key={group.id}
           className="p-4 bg-white border rounded-xl"
         >
-          <div className="flex -space-x-2">
-            {group.members.slice(0, 3).map((member) => (
-              <Avatar
-                key={member.id}
-                username={member.user?.name}
-                profileImage={member.user?.profileImage}
-                size={28}
-              />
-            ))}
-
-            {group.members.length > 3 && (
-              <div className="flex items-center justify-center text-xs bg-gray-200 border rounded-full w-7 h-7">
-                +{group.members.length - 3}
-              </div>
-            )}
-          </div>
           <div className="mt-3">
             <h2 className="font-semibold">{group.name}</h2>
             <p className="text-sm text-gray-500">{group.topic}</p>
